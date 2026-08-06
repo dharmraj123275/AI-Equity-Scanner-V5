d8f8d0488235c7e0ff3558bb074e2ac00a9577ab
dharmraj123275 <dtchudasama21@gmail.com>
2026-08-07 04:02:53 +0530

    Update upstox.js

--- services/upstox.js
+++ services/upstox.js
@@ -1,1 +1,38 @@
+const axios = require("axios");
 
+const BASE_URL = "https://api.upstox.com/v2";
+
+const ACCESS_TOKEN = process.env.UPSTOX_ACCESS_TOKEN;
+
+async function getQuote(instrumentKey) {
+
+    try {
+
+        const response = await axios.get(
+            `${BASE_URL}/market-quote/quotes`,
+            {
+                headers: {
+                    Accept: "application/json",
+                    Authorization: `Bearer ${ACCESS_TOKEN}`
+                },
+                params: {
+                    instrument_key: instrumentKey
+                }
+            }
+        );
+
+        return response.data;
+
+    } catch (error) {
+
+        console.error("Upstox Error:", error.response?.data || error.message);
+
+        throw error;
+
+    }
+
+}
+
+module.exports = {
+    getQuote
+};
